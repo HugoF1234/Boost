5aee9ab48547203a5f4a991524408f7120a5fd61
HugoF1234 <167479132+HugoF1234@users.noreply.github.com>
2025-05-15 18:05:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -29,6 +29,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = db_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+with app.app_context():
+    db.create_all()
+
 # -----------------------
 # MODÈLES SQLALCHEMY
 # -----------------------
